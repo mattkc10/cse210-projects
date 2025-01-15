05800bb98206ae12c2b76ae7c8fcfcd8f38f93ee
yourusername~ <youremail@>
2025-01-14 19:26:54 -0700

    Corrections

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -13,27 +13,24 @@ class Program
         Console.WriteLine("Now guess a number.");
         string guessGiven = Console.ReadLine();
 
-        int guess = int.Parse(guessGiven);
+        int guess = -1;
 
         while (guess != answer)
         {   
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
             if (guess > answer)
             {
                 Console.WriteLine("Too high. Guess again.");
-                string guessGiven = Console.ReadLine();
-
-                int guess = int.Parse(guessGiven);
             }
         
-            if (guess < answer)
+            else if (guess < answer)
             {
                 Console.WriteLine("Too low. Guess again");
-                string guessGiven = Console.ReadLine();
-
-                int guess = int.Parse(guessGiven);
             }
 
-            if (guess == answer) 
+            else (guess == answer) 
             { 
                 Console.WriteLine("You guessed it!");
             }
