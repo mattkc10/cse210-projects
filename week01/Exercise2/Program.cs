fc33e67cad6abbb5f03d3bfd94014c06b3384dfb
yourusername~ <youremail@>
2025-01-14 17:34:07 -0700

    First half of exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your GPA as a whole number.");
+        string gradeFromUser = Console.ReadLine();
+
+        int x = int.Parse(gradeFromUser);
+
+        if (x < 90)
+        {
+            Console.WriteLine("This is an A or higher!");
+            Console.WriteLine("Great job! You are passing!");
+        }
+        else if (x < 79 && x > 90)
+        {
+            Console.WriteLine("This is an B grade!");
+            Console.WriteLine("Great job! You are passing!");
+        }
+        else if (x < 69 && x > 80)
+        {
+            Console.WriteLine("This is at a C.");
+            Console.WriteLine("Great job! You are passing!");
+        }
+        else if (x < 60 && x > 70)
+        {
+            Console.WriteLine("This is a D grade.");
+            Console.WriteLine("You do not have a passing grade. Work hard, you can do it!");
+        }
+        else if (x > 60)
+        {
+            Console.WriteLine("This is a F grade.");
+            Console.WriteLine("You do not have a passing grade. Work hard, you can do it!");
+        }
     }
 }
