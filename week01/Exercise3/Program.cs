62e99f959d975df4362de18745dc306e0a555e1b
yourusername~ <youremail@>
2025-01-14 19:10:58 -0700

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Lets play a game. Tell me what the magic number is?");
+        string answerGiven = Console.ReadLine();
+
+        int answer = int.Parse(answerGiven);
+
+
+        Console.WriteLine("Now guess a number.");
+        string guessGiven = Console.ReadLine();
+
+        int guess = int.Parse(guessGiven);
+
+        while (guess != answer)
+        if (guess > answer)
+        {
+            Console.WriteLine("Too high");
+        }
+        if (guess < answer)
+        {
+            Console.WriteLine("Too low");
+        }
+        else (guess == answer)
+        {
+            Console.WriteLine("You guess it!");
+        }
+
     }
 }
