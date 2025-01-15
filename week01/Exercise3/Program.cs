cf690b0ac9e9c88437dcfca9f6809dc48d243361
yourusername~ <youremail@>
2025-01-14 19:31:58 -0700

    Part 3 code

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,10 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Lets play a game. Tell me what the magic number is?");
-        string answerGiven = Console.ReadLine();
-
-        int answer = int.Parse(answerGiven);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
 
         Console.WriteLine("Now guess a number.");
@@ -16,16 +14,16 @@ class Program
         int guess = -1;
 
         // We could also use a do-while loop here...
-        while (answer != guess)
+        while (magicNumber != guess)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
-            if (answer > guess)
+            if (magicNumber > guess)
             {
                 Console.WriteLine("Higher");
             }
-            else if (answer < guess)
+            else if (magicNumber < guess)
             {
                 Console.WriteLine("Lower");
             }
