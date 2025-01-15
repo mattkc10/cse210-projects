17a41028906410f3ad40f3c3b915685a19593493
yourusername~ <youremail@>
2025-01-14 21:15:25 -0700

    Square root

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -17,13 +17,25 @@ class Program
 
     string fullName = combineNames(first, last);
 
-
+    Console.WriteLine("Please tell me which is your favorite number. ");
+    double number = Convert.ToDouble(Console.ReadLine()); 
+    double result = GetSquareRoot(number); 
+    
+    
+    
     static string combineNames(string first, string last)
     {
         return first + "" + last;
     }
-        Console.WriteLine(Message);
-        Console.WriteLine(fullName);
+
+    static double GetSquareRoot(double number)
+    {
+        return Math.Sqrt(number);
+    }
+
+    Console.WriteLine(Message);
+    Console.WriteLine(fullName);
+    Console.WriteLine("The square root of " + number + " is: " + result); 
     }
     
 
