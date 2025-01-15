1e6715aa4d6ce6c7b8ee61790fc175f9dfe04c29
yourusername~ <youremail@>
2025-01-14 20:50:26 -0700

    Exercise 5 code

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,7 +3,26 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    
+        static void Message(string[] args)
+        {
+            Console.WriteLine("Welcome to the program!");    
+        }
+
+        static void Name(string first, string last)
+        {
+            Console.WriteLine("What is your first name?");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string last = Console.ReadLine();
+
+            string name = first + last;
+            return name;
+        }
+    Console.WriteLine(Message);
+    Console.WriteLine(Name);
     }
+    
+
 }
