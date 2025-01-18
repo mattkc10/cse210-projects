7df7bf991665eceef213a2c5b065807abc1e6ceb
yourusername~ <youremail@>
2025-01-17 20:53:43 -0700

    Resume activity

--- week02/Resume.cs
+++ week02/Resume.cs
@@ -1,5 +1,18 @@
+using System;
+
 public class Resume
 {
     public string _name;
-    public List<Job> _job = new List<Job>();
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (job job in _jobs)
+        {
+            job.Display();
+        }
+    }
 }
